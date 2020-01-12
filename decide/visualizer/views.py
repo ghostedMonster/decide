10cfa4cb4d143bbac8b9c67b508af35f3b1b1cb6
Taxo16 <taxo.pianista@gmail.com>
2020-01-12 10:44:28 +0100

    Añadidos datos del bypass a la visualización en PDF

--- decide/visualizer/views.py
+++ decide/visualizer/views.py
@@ -35,18 +35,39 @@ class VisualizerView(TemplateView):
         return render(request, 'visualizer/visualizador.html', {'porcentajes': porcentajes, 'votos': votos, 'votantes':votantes, 'desviacion':desviacion})
 
     def descargaPDF(request):
-        # Create the HttpResponse object with the appropriate PDF headers.
+        
+        ## Create the HttpResponse object with the appropriate PDF headers.
         response = HttpResponse(content_type='application/pdf')
         response['Content-Disposition'] = 'attachment; filename="datos_decide.pdf"'
 
-        # Create the PDF object, using the response object as its "file."
+        ## Create the PDF object, using the response object as its "file."
         p = canvas.Canvas(response)
 
-        # Draw things on the PDF. Here's where the PDF generation happens.
-        # See the ReportLab documentation for the full list of functionality.
-        p.drawString(100, 100, "Aquí se mostrarán los datos en PDF")
+        ## Draw things on the PDF. Here's where the PDF generation happens.
+        ## See the ReportLab documentation for the full list of functionality.
+        #p.drawImage(archivo_imagen, 40, 750, 120, 90,preserveAspectRatio=True)
+        
+        #Establecemos el tamaño de letra en 16 y el tipo de letra Helvetica
+        p.setFont("Helvetica", 16)
+        #Dibujamos una cadena en la ubicación X,Y especificada
+        p.drawString(230, 790, u"PLATAFORMA DECIDE")
+        p.setFont("Helvetica", 14)
+        p.drawString(200, 770, u"REPORTE DE DATOS DE VOTANTES")
+        p.setFont("Helvetica", 10)
+        p.drawString(100, 720, u"Porcentaje 1: 0.25")
+        p.setFont("Helvetica", 10)
+        p.drawString(100, 690, u"Porcentaje 2: 0.75")
+        p.setFont("Helvetica", 10)
+        p.drawString(100, 660, u"Votos: 1357")
+        p.setFont("Helvetica", 10)
+        p.drawString(100, 630, u"Votantes: 1521")
+        p.setFont("Helvetica", 10)
+        p.drawString(100, 600, u"Desviación: 0.321457")
 
-        # Close the PDF object cleanly, and we're done.
+        ## Close the PDF object cleanly, and we're done.
         p.showPage()
         p.save()
-        return response
+        return response              
+         
+    
+    
